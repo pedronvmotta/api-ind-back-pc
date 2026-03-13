464b381cbf32abbeb6047ddef4f4e8c142eddacb
pedronvmotta <pedronaves.20241@poli.ufrj.br>
2026-03-13 17:29:24 -0300

    commitando

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClienteModule } from './cliente/cliente.module';
+import { PizzaModule } from './pizza/pizza.module';
+import { EnderecoModule } from './endereco/endereco.module';
+import { ClienteModule } from './cliente/cliente.module';
 
 @Module({
-  imports: [ClienteModule],
+  imports: [ClienteModule, EnderecoModule, PizzaModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/pizza/pizza.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePizzaDto } from './dto/create-pizza.dto';
+import { UpdatePizzaDto } from './dto/update-pizza.dto';
+
+@Injectable()
+export class PizzaService {
+  create(createPizzaDto: CreatePizzaDto) {
+    return 'This action adds a new pizza';
+  }
+
+  findAll() {
+    return `This action returns all pizza`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} pizza`;
+  }
+
+  update(id: number, updatePizzaDto: UpdatePizzaDto) {
+    return `This action updates a #${id} pizza`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} pizza`;
+  }
+}
